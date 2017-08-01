0b175d02148cb0523e5c0d208644c096dba0adc0
Jordan Hiertz <jordan.hiertz@al-enterprise.com>
2017-08-01 15:12:44 +0200

    #28357: Add documentation to the class (required by jsdoc)

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,21 +3,22 @@ module.exports = function (grunt) {
     jsdoc2md: {
       separateOutputFiles: {
         files: [
-            { src: "./lib/common/Error.js", dest: "build/common/error.md" },
-            { src: "./lib/common/Events.js", dest: "build/common/events.md" },
-            { src: "./lib/common/Logger.js", dest: "build/common/logger.md" },
-            { src: "./lib/common/XMPPUtils.js", dest: "build/common/xmpputils.md" },
-            { src: "./lib/config/config.js", dest: "build/config/config.md" },
-            { src: "./lib/config/Options.js", dest: "build/config/options.md" },
-            { src: "./lib/connection/HttpService.js", dest: "build/connection/httpservice.md" },
-            { src: "./lib/connection/RESTService.js", dest: "build/connection/restservice.md" },
-            { src: "./lib/connection/XMPPService.js", dest: "build/connection/xmppservice.md" },
-            { src: "./lib/services/Bubbles.js", dest: "build/services/bubbles.md" },
-            { src: "./lib/services/Contacts.js", dest: "build/services/contacts.md" },
-            { src: "./lib/services/IM.js", dest: "build/services/im.md" },
-            { src: "./lib/services/Presence.js", dest: "build/services/presence.md" },
-            { src: "./lib/Core.js", dest: "build/core.md" },
-            { src: "./lib/Proxy.js", dest: "build/proxy.md" }
+            { src: "lib/common/Error.js", dest: "build/common/error.md" },
+            { src: "lib/common/Events.js", dest: "build/common/events.md" },
+            { src: "lib/common/Logger.js", dest: "build/common/logger.md" },
+            { src: "lib/common/XMPPUtils.js", dest: "build/common/xmpputils.md" },
+            { src: "lib/config/config.js", dest: "build/config/config.md" },
+            { src: "lib/config/Options.js", dest: "build/config/options.md" },
+            { src: "lib/connection/HttpService.js", dest: "build/connection/httpservice.md" },
+            { src: "lib/connection/RESTService.js", dest: "build/connection/restservice.md" },
+            { src: "lib/connection/XMPPService.js", dest: "build/connection/xmppservice.md" },
+            { src: "lib/services/Admin.js", dest: "build/services/admin.md" },
+            { src: "lib/services/Bubbles.js", dest: "build/services/bubbles.md" },
+            { src: "lib/services/Contacts.js", dest: "build/services/contacts.md" },
+            { src: "lib/services/IM.js", dest: "build/services/im.md" },
+            { src: "lib/services/Presence.js", dest: "build/services/presence.md" },
+            { src: "lib/Core.js", dest: "build/core.md" },
+            { src: "lib/Proxy.js", dest: "build/proxy.md" }
         ]
       }
     },
