f3ac358fee8f2c385f7d06fccb98ef74f36a357d
vincent.berder@al-enterprise.com <vincent.berder@al-enterprise.com>
2018-02-01 21:52:01 +0100

    psswd

--- afterbuild/rainbowNodeSdk.js
+++ afterbuild/rainbowNodeSdk.js
@@ -11,7 +11,7 @@ let optionsSDK = {
     },
     "credentials": {
         "login": "vincent00@vbe.test.openrainbow.net",  // The Rainbow email account to use
-        "password": "XXXXXX"   // The Rainbow associated password to use
+        "password": "Alcatel123!"   // The Rainbow associated password to use
 //        "login": "vincent.berder@al-enterprise.com",  // The Rainbow email account to use
 //        "password": ""   // The Rainbow associated password to use
     },
