df05e82f65b0ca11275fecfaa22b5d2b03a79e3f
Vincent Bailleau <vincent.bailleau@al-enterprise.com>
2018-03-02 23:20:53 +0100

    puppeteer update

--- puppeteer.js
+++ puppeteer.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 
 (async() => {
     try {
-        const browser = await puppeteer.launch({
+        const browser = await puppeteer.launch({headless: true, 
             args: ['--no-sandbox', '--disable-setuid-sandbox']
         });
         const page = await browser.newPage();
