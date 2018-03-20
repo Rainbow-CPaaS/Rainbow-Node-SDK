89b49e2c24371d5d357dd4693e2b4408e1e4f53a
vincent.berder@al-enterprise.com <vincent.berder@al-enterprise.com>
2018-03-20 09:35:27 +0100

    prepare version v1.38.0

--- test/options.js
+++ test/options.js
@@ -1,20 +1,72 @@
 let options = {
+    /*
     "rainbow": {
-      "host": "dummy.openrainbow.org", // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
-    },
-    "xmpp": {
-      "protocol": "ws",
-      "host": "dummy.openrainbow.org",
-      "port": 80
+        "host": "vberder.openrainbow.org",                      // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
+//        "host": "sandbox",                      // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
+//        "host": "web-sandbox.openrainbow.com",                      // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
     },
     "credentials": {
-      "login": "alice@pgu.test.openrainbow.net", // The Rainbow email account to use
-      "password": "dummy" // The Rainbow associated password to use
+        "login": "alice@pgu.test.openrainbow.net",  // The Rainbow email account to use
+//        "login": "vincent00@vbe.test.openrainbow.net",  // The Rainbow email account to use
+//        "password": "Alcatel123!"   // The Rainbow associated password to use
+        "password": "Password_123"   // The Rainbow associated password to use
+//        "login": "vincent.berder@al-enterprise.com",  // The Rainbow email account to use
+//        "password": ""   // The Rainbow associated password to use
     },
     // Application identifier
     "application": {
-      "appID": "59719d166260a9f00c211fc9", // The Rainbow Application Identifier - application must have a 'deployed' state
-      "appSecret": "Oz8cbbElggWcKgrwhSBaL9C9", // The Rainbow Application Secret - retrieved from developer hub
+        "appID": "dce4508010dc11e89911ade160c1b521", // The Rainbow Application Identifier - application must have a 'deployed' state
+        "appSecret": "XjJZLOvVCfvcpPTmt0UtIRUQddKfCGbDGBEfdB3qfXvZCKpgb9k0HaxzfQxI6UAa", // The Rainbow Application Secret - retrieved from developer hub
+    },
+
+    // Proxy configuration
+    proxy: {
+        host: '192.168.254.49',
+        port: 8080,
+        protocol: 'http'
+    },
+
+    "xmpp": {
+        "protocol": "ws",
+//        "host": "dummy.openrainbow.org",
+        "host": "vberder.openrainbow.org",
+        "port": 5280
+    },
+
+    // Logs options
+    "logs": {
+        "enableConsoleLogs": true,              // Default: true
+        "enableFileLogs": false,                // Default: false
+        "file": {
+            "path": '/var/tmp/rainbowsdk/',     // Default path used
+            "level": 'debug'                    // Default log level used
+        }
+    },
+    // IM options
+    "im": {
+        "sendReadReceipt": true   // True to send the 'read' receipt automatically
+    } // */
+    "rainbow": {
+        "host": "dummy.openrainbow.org" // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
+//        "host": "vberder.openrainbow.org", // Can be "sandbox" (developer platform), "official" or any other hostname when using dedicated AIO
+    },
+    "xmpp": {
+      "protocol": "ws",
+        "host": "dummy.openrainbow.org",
+//        "host": "vberder.openrainbow.org",
+        "port": 80
+    },
+    "credentials": {
+      "login": "alice@pgu.test.openrainbow.net", // The Rainbow email account to use
+//        "password": "Password_123" // The Rainbow associated password to use
+        "password": "dummy" // The Rainbow associated password to use
+    },
+    // Application identifier
+    "application": {
+         "appID": "59719d166260a9f00c211fc9", // The Rainbow Application Identifier - application must have a 'deployed' state
+         "appSecret": "Oz8cbbElggWcKgrwhSBaL9C9", // The Rainbow Application Secret - retrieved from developer hub
+//        "appID": "dce4508010dc11e89911ade160c1b521", // The Rainbow Application Identifier - application must have a 'deployed' state
+//        "appSecret": "XjJZLOvVCfvcpPTmt0UtIRUQddKfCGbDGBEfdB3qfXvZCKpgb9k0HaxzfQxI6UAa", // The Rainbow Application Secret - retrieved from developer hub
     },
     // Logs options
     "logs": {
@@ -28,7 +80,7 @@ let options = {
     // IM options
     "im": {
       "sendReadReceipt": true // True to send the 'read' receipt automatically
-    }
+    } // */
   };
 
   module.exports = options;
