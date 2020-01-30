0fd2bca033feddbdc3683d4f088542d10e541454
berder <vincent.berder@al-enterprise.com>
2020-01-30 22:31:13 +0100

    update src\ Sample\index.ts

--- src/Samples/index.ts
+++ src/Samples/index.ts
@@ -1,24 +1,11 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function(thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function(resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function(resolve, reject) {
-        function fulfilled(value) { try {
-            step(generator.next(value));
-        }
-        catch (e) {
-            reject(e);
-        } }
-        function rejected(value) { try {
-            step(generator.throw(value));
-        }
-        catch (e) {
-            reject(e);
-        } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
+/*
+ * @name index.ts
+ *
+ * @description :
+ * The index.ts file is not a "best practice", but it is a file used by developper to test/validate the SD, so you can find in it some help.
+ *
+ */
 // Load the SDK
 // For using the fiddler proxy which logs requests to server
 // process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
