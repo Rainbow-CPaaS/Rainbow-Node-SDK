ffe9734c51b07d61f20f20098eaee9a8409f9fe4
vberder <vincent.berder@al-enterprise.com>
2021-10-06 23:20:14 +0200

    Jenkinsfile-sts

--- Jenkinsfile-sts.groovy
+++ Jenkinsfile-sts.groovy
@@ -133,7 +133,7 @@ pipeline {
                 when {
                     allOf {
                         branch "STSDelivery"; 
-                        triggeredBy 'user'
+                        triggeredBy cause($class: 'jenkins.branch.BranchIndexingCause')
                     }
                 }
                 steps{
