6b7eb2aec89737bf9f0f3aa22ee9f529fefa2c2f
vberder <vincent.berder@al-enterprise.com>
2022-01-27 15:48:09 +0100

    Jenkinsfile-sts

--- Jenkinsfile-sts.groovy
+++ Jenkinsfile-sts.groovy
@@ -399,7 +399,7 @@ pipeline {
                         stage("Generate documentation search index") {
                             try {
                                 echo "Build Hub V2 search index : "
-                                   unstash 'Documentation'
+                                   unstash 'DocumentationFolder'
                                    sh script: """
                                    
                                 echo "folder where run the Build Hub V2 search index."
