fd1062ca8089e72ce88bde1a5567b157584ce3ac
vberder <vincent.berder@al-enterprise.com>
2022-04-08 10:49:23 +0200

    Jenkinsfile-sts

--- Jenkinsfile-sts.groovy
+++ Jenkinsfile-sts.groovy
@@ -16,7 +16,7 @@ def DOC_PATH = ''
 pipeline {
     agent {
         label {
-                  label "docker-slave-cpaas-buster"
+                  label "docker-slave-cpaas-bullseye"
                   customWorkspace "/home/jenkins/workspace/SDK-Node-SDK-sts_delivery"
         }        
     }
