390f8f9d0499741ec41408fa47b7559511543b13
vberder <vincent.berder@al-enterprise.com>
2023-12-04 11:43:05 +0100

    doc update

--- src/lib/NodeSDK.ts
+++ src/lib/NodeSDK.ts
@@ -50,28 +50,27 @@ let LOG_ID = "NodeSDK/IDX";
  * @property {string} options.s2s.locallistenningport "4000", Local port where the events must be forwarded from S2S Callback Web server.
  * @property {string} options.rest.useRestAtStartup, enable the REST requests to the rainbow server at startup (used with startWSOnly method). Default value is true.
  * @property {string} options.rest.useGotLibForHttp, allows to enable the use of `got` lib for REST requests (esle the old Request lib is used). Default value is true.
- * @property {string} options.rest.gotOptions, allows to customize the `got` lib for REST requests options. Default value is :
- *  {
- *
- * //Keep sockets around in a pool to be used by other requests in the future. Default = false
- * keepAlive: true, // ?: boolean | undefined;
- *
- * //When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000.
- * //Only relevant if keepAlive is set to true.
- * keepAliveMsecs: 501, // ?: number | undefined;
- * Maximum number of sockets to allow per host. Default for Node 0.10 is 5, default for Node 0.12 is Infinity
- * maxSockets: 26, // ?: number | undefined;
- *
- * Maximum number of sockets allowed for all hosts in total. Each request will use a new socket until the maximum is reached. Default: Infinity.
- * maxTotalSockets: Infinity, // ?: number | undefined;
- *
- * Maximum number of sockets to leave open in a free state. Only relevant if keepAlive is set to true. Default = 256.
- * maxFreeSockets: 1001, // ?: number | undefined;
- *
- * Socket timeout in milliseconds. This will set the timeout after the socket is connected.
- * timeout: 60001 , // ?: number | undefined;
- *
- * }
+ * @property {string} options.rest.gotOptions, allows to customize the `got` lib for REST requests options. Default value is :</BR>
+ *  {</BR>
+ * //Keep sockets around in a pool to be used by other requests in the future. Default = false</BR>
+ * keepAlive: true, // ?: boolean or undefined;</BR>
+ * </BR>
+ * //When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000.</BR>
+ * //Only relevant if keepAlive is set to true.</BR>
+ * keepAliveMsecs: 501, // ?: number or undefined;</BR>
+ * </BR>
+ * Maximum number of sockets to allow per host. Default for Node 0.10 is 5, default for Node 0.12 is Infinity</BR>
+ * maxSockets: 26, // ?: number or undefined;</BR>
+ * </BR>
+ * Maximum number of sockets allowed for all hosts in total. Each request will use a new socket until the maximum is reached. Default: Infinity.</BR>
+ * maxTotalSockets: Infinity, // ?: number or undefined;</BR>
+ * </BR>
+ * Maximum number of sockets to leave open in a free state. Only relevant if keepAlive is set to true. Default = 256.</BR>
+ * maxFreeSockets: 1001, // ?: number or undefined;</BR>
+ * </BR>
+ * Socket timeout in milliseconds. This will set the timeout after the socket is connected.</BR>
+ * timeout: 60001 , // ?: number or undefined;</BR>
+ * }</BR>
  *
  * @property {string} options.credentials.login "user@xxxx.xxx", The Rainbow email account to use.
  * @property {string} options.credentials.password "XXXXX", The password.
@@ -382,28 +381,27 @@ class NodeSDK {
      * @param {string} options.s2s.locallistenningport "4000", Local port where the events must be forwarded from S2S Callback Web server.
      * @param {string} options.rest.useRestAtStartup enable the REST requests to the rainbow server at startup (used with startWSOnly method). default value is true.
      * @param {string} options.rest.useGotLibForHttp allows to enable the use of `got` lib for REST requests (esle the old Request lib is used). Default value is true.
-     * @param {string} options.rest.gotOptions, allows to customize the `got` lib for REST requests options. Default value is :
-     *  {
-     *
-     * //Keep sockets around in a pool to be used by other requests in the future. Default = false
-     * keepAlive: true, // ?: boolean | undefined;
-     *
-     * //When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000.
-     * //Only relevant if keepAlive is set to true.
-     * keepAliveMsecs: 501, // ?: number | undefined;
-     * Maximum number of sockets to allow per host. Default for Node 0.10 is 5, default for Node 0.12 is Infinity
-     * maxSockets: 26, // ?: number | undefined;
-     *
-     * Maximum number of sockets allowed for all hosts in total. Each request will use a new socket until the maximum is reached. Default: Infinity.
-     * maxTotalSockets: Infinity, // ?: number | undefined;
-     *
-     * Maximum number of sockets to leave open in a free state. Only relevant if keepAlive is set to true. Default = 256.
-     * maxFreeSockets: 1001, // ?: number | undefined;
-     *
-     * Socket timeout in milliseconds. This will set the timeout after the socket is connected.
-     * timeout: 60001 , // ?: number | undefined;
-     *
-     * }
+     * @param {string} options.rest.gotOptions, allows to customize the `got` lib for REST requests options. Default value is : </BR>
+     *  {</BR>
+     * //Keep sockets around in a pool to be used by other requests in the future. Default = false</BR>
+     * keepAlive: true, // ?: boolean or undefined;</BR>
+     * </BR>
+     * //When using HTTP KeepAlive, how often to send TCP KeepAlive packets over sockets being kept alive. Default = 1000.</BR>
+     * //Only relevant if keepAlive is set to true.</BR>
+     * keepAliveMsecs: 501, // ?: number or undefined;</BR>
+     * </BR>
+     * Maximum number of sockets to allow per host. Default for Node 0.10 is 5, default for Node 0.12 is Infinity</BR>
+     * maxSockets: 26, // ?: number or undefined;</BR>
+     * </BR>
+     * Maximum number of sockets allowed for all hosts in total. Each request will use a new socket until the maximum is reached. Default: Infinity.</BR>
+     * maxTotalSockets: Infinity, // ?: number or undefined;</BR>
+     * </BR>
+     * Maximum number of sockets to leave open in a free state. Only relevant if keepAlive is set to true. Default = 256.</BR>
+     * maxFreeSockets: 1001, // ?: number or undefined;</BR>
+     * </BR>
+     * Socket timeout in milliseconds. This will set the timeout after the socket is connected.</BR>
+     * timeout: 60001 , // ?: number or undefined;</BR>
+     * }</BR>
      *
      * @param {string} options.credentials.login "user@xxxx.xxx", The Rainbow email account to use.
      * @param {string} options.credentials.password "XXXXX", The password.
