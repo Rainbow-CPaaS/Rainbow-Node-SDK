9d64d59daf2d2b872d71fb8ca7380cf64b770aa0
vberder <vincent.berder@al-enterprise.com>
2023-12-11 14:04:03 +0100

    useRequestRateLimiter

--- src/Samples/index.ts
+++ src/Samples/index.ts
@@ -241,7 +241,7 @@ let urlS2S;
         "httpoverxmppserver": true,
         "intervalBetweenCleanMemoryCache": 1000 * 60 * 60 * 6, // Every 6 hours.
         "requestsRate": {
-            "useRequestRateLimiter": false,
+            "useRequestRateLimiter": true,
             "maxReqByIntervalForRequestRate": 50, // nb requests during the interval.
             "intervalForRequestRate": 60, // nb of seconds used for the calcul of the rate limit.
             "timeoutRequestForRequestRate": 600 // nb seconds Request stay in queue before being rejected if queue is full.
