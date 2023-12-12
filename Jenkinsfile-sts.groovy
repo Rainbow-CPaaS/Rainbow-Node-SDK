5466fd14dc243b1cc40f5c8dc85a7ee0e8b4942d
vberder <vincent.berder@al-enterprise.com>
2023-12-12 17:43:55 +0100

    Jenkinsfile-lts Jenkinsfile-sts

--- Jenkinsfile-sts.groovy
+++ Jenkinsfile-sts.groovy
@@ -22,7 +22,7 @@ pipeline {
     }
     options {
         timeout(time: 1, unit: 'HOURS') 
-        disableConcurrentBuilds(),
+        disableConcurrentBuilds()
         //withCredentials()
         buildDiscarder(logRotator(
             numToKeepStr: '30',
