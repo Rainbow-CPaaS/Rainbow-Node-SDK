194f5538aff442b48161953609d7fc16f68a7aa7
vberder <vincent.berder@al-enterprise.com>
2024-04-02 14:55:25 +0200

    Jenkinsfile-sts.groovy

--- Jenkinsfile-sts.groovy
+++ Jenkinsfile-sts.groovy
@@ -422,13 +422,16 @@ pipeline {
                                 """
                                 // unstash "withBuildDir"
 
-                                echo "installation npm"
-                                sh "npm install developers_searchindex --registry https://nexus.openrainbow.io/repository/npm-dev"
-                                sh "npm list developers_searchindex  --registry https://nexus.openrainbow.io/repository/npm-dev"
+                                echo "Installation of developers_searchindex HUB V2 search library."
+                                sh """
+                                cd "${workspace}/Documentation"
+                                npm install developers_searchindex --registry https://nexus.openrainbow.io/repository/npm-dev
+                                npm list developers_searchindex  --registry https://nexus.openrainbow.io/repository/npm-dev
+                                """
 
                                 echo "build hub doc"
                                 sh script: """
-                                cd "${workspace}/Documentation"
+                                # cd "${workspace}/Documentation"
                                 npm exec -- developers_searchindex --docPath Documentation/doc/sdk/node/sts"
                                 # sh "npx developers_searchindex --docPath build/doc/hub"
                                 ls -la build/doc/hub
