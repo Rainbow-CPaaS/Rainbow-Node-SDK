1768f7f3d63d661789286ffd74d4c0e5529d126c
vberder <vincent.berder@al-enterprise.com>
2026-04-16 11:20:31 +0200

    update wait-sync

--- wait-sync.bat
+++ wait-sync.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Waiting 3 seconds for file system sync...
+echo Waiting 7 seconds for file system sync...
 :: Using ping to create a reliable delay on Windows
-ping 127.0.0.1 -n 4 > nul
+ping 127.0.0.1 -n 8 > nul
 echo Ready.
